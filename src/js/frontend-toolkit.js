55b960f6e9dd856bd12de6b5a7e476c3cfbf8eb0
Casper Houde <casper@reprovinci.nl>
2021-06-25 10:15:21 +0200

    Added modal

--- src/js/frontend-toolkit.js
+++ src/js/frontend-toolkit.js
@@ -1,5 +1,7 @@
 import './components/fixednav';
 import './components/swiper';
+import 'bootstrap/js/src/modal';
+import 'bootstrap/js/src/collapse';
 
 const message = 'Hier kan javascript komen.';
 console.log(message);
