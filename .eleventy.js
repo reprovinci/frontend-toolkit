b39621771e658140a49dce80ac32a764777210fc
Casper Houde <casper@reprovinci.nl>
2021-07-09 15:18:00 +0200

    Added icons

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,19 @@
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const { DateTime } = require("luxon");
+const Prism = require('prismjs');
+const loadLanguages = require('prismjs/components/');
+const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPassthroughCopy('dist');
   eleventyConfig.setUseGitIgnore(false);
+
+  eleventyConfig.addFilter('highlight', function (myVariable, lang = 'html') {
+    let html = Prism.highlight(myVariable, Prism.languages[lang], lang);
+    html = `<pre class="language-${lang}"><code class="language-${lang}">${html}</code></pre>`;
+
+    return html;
+  });
 
   eleventyConfig.addCollection('nav', function (collection) {
     return collection.getFilteredByTag('menu')
@@ -12,11 +21,12 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addFilter('date', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).setLocale('nl-NL').toLocaleString(DateTime.DATE_MED);
+    return DateTime.fromJSDate(dateObj)
+      .setLocale('nl-NL')
+      .toLocaleString(DateTime.DATE_MED);
   });
 
   eleventyConfig.addFilter('stringify', function (value) {
-    console.log(value);
     if (value && typeof value.to_liquid === 'function') {
       return stringify(value.to_liquid());
     }
